e7ad5bc5223673af0303a0647d60749417e9286d
JTim <jiuntim.lin@gmail.com>
2020-08-14 14:10:29 -0700

    fix mlarge.mlarge_model bug 2-4

--- src/mlarge/mlarge_model.py
+++ src/mlarge/mlarge_model.py
@@ -277,6 +277,7 @@ class feature_gen(keras.utils.Sequence):
                 eqlon=eqinfo[2]
                 eqlat=eqinfo[3]
                 #eqlon,eqlat=get_hypo(logfile)
+                print('Input E:',E)
                 print('rndEQIDX=%s,ID=%s, ID_from_EQinfo=%s eqlon,eqlat=%f %f'%(int(rndEQidx[0]),E[int(rndEQidx[0])],eqinfo[0],eqlon,eqlat))
                 #########################################################
                 #if check_PGDs_hypoInfo(Data,STA,hypo=[eqlon,eqlat],dist_thres=5.0,min_Nsta=8)==False:
